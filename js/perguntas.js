f6a559818b81247e9f6b17092072c3c73b809b18
Eehrst <114171408+Eehrst@users.noreply.github.com>
2024-09-06 23:17:53 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,173 @@
+export const perguntas = [
+    {
+        enunciado: "Qual das seguintes práticas contribui para uma boa qualidade de sono?",
+        alternativas: [
+            {
+                texto: "Manter uma rotina de sono regular",
+                afirmacao: [
+                    "Dormir bem é fundamental para a recuperação do corpo e da mente.",
+                    "Uma boa noite de sono melhora a concentração e a memória."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Criar um ambiente tranquilo e escuro.",
+                afirmacao: [
+                    "Manter uma rotina de sono regular ajuda a regular o relógio biológico.",
+                    "O sono de qualidade fortalece o sistema imunológico."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Por que é importante evitar telas eletrônicas antes de dormir?",
+        alternativas: [
+            {
+                texto: "A luz azul emitida pode interferir na produção de melatonina.",
+                afirmacao: [
+                    "Criar um ambiente tranquilo e escuro favorece um sono mais profundo.",
+                    "Evitar telas eletrônicas antes de dormir pode melhorar a qualidade do sono.",
+                    "A prática de relaxamento antes de deitar ajuda a adormecer mais rapidamente."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "O estímulo visual pode dificultar o relaxamento e a indução do sono.",
+                afirmacao: [
+                    "A privação de sono pode afetar negativamente o humor e o bem-estar emocional.",
+                    "Um colchão e travesseiro adequados são essenciais para um bom descanso.",
+                    "A ingestão de cafeína à noite pode prejudicar a qualidade do sono."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Quais são os benefícios de um colchão e travesseiro adequados?",
+        alternativas: [
+            {
+                texto: "Oferecem suporte adequado e conforto durante o sono.",
+                afirmacao: [
+                    "Exercícios físicos regulares contribuem para um sono mais reparador.",
+                    "Estabelecer um horário consistente para dormir e acordar promove melhores noites de sono.",
+                    "Evitar grandes refeições antes de dormir pode prevenir desconfortos noturnos"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Previnem dores nas costas e no pescoço.",
+                afirmacao: [
+                    "O sono é crucial para a regeneração dos músculos após atividades físicas intensas.",
+                    "Uma temperatura ambiente confortável ajuda a melhorar a qualidade do sono.",
+                    "O sono adequado pode melhorar a capacidade de tomar decisões e resolver problemas."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Como o exercício físico regular afeta o sono?",
+        alternativas: [
+            {
+                texto: "Melhora a qualidade do sono ao promover o relaxamento.",
+                afirmacao: [
+                    "A meditação e a leitura são práticas que podem facilitar a transição para o sono.",
+                    "Dormir bem é essencial para o desenvolvimento e crescimento saudável, especialmente em crianças."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Contribui para uma rotina de sono mais regular.",
+                afirmacao: [
+                    "A falta de sono pode aumentar o risco de doenças crônicas, como diabetes e hipertensão.",
+                    "Técnicas de respiração profunda podem ajudar a relaxar antes de dormir."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é a influência da cafeína sobre o sono?",
+        alternativas: [
+            {
+                texto: "Pode atrasar o início do sono.",
+                afirmacao: [
+                    "Limitar o consumo de álcool pode prevenir distúrbios do sono.",
+                    "O estresse e a ansiedade podem ser mitigados com um sono de qualidade."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Reduz a qualidade do sono ao aumentar a vigília.",
+                afirmacao: [
+                    "A exposição à luz natural durante o dia ajuda a regular o ciclo de sono.",
+                    "Uma rotina noturna relaxante pode sinalizar ao corpo que é hora de descansar."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Quais são dois benefícios de uma temperatura ambiente confortável durante o sono?",
+        alternativas: [
+            {
+                texto: "Melhora a qualidade do sono.",
+                afirmacao: [
+                    "Dormir o suficiente pode melhorar a aparência da pele e a saúde geral.",
+                    "Evitar cochilos prolongados durante o dia pode ajudar a manter o sono noturno."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Facilita a transição para um sono mais profundo.",
+                afirmacao: [
+                    "A qualidade do sono pode ser monitorada com o uso de dispositivos de rastreamento do sono.",
+                    "Criar um ritual de sono, como um banho quente, pode facilitar o relaxamento."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Como uma alimentação equilibrada pode influenciar o sono?",
+        alternativas: [
+            {
+                texto: "Evitar grandes refeições antes de dormir previne desconfortos noturnos.",
+                afirmacao: [
+                    "A falta de sono pode afetar negativamente a produtividade e o desempenho no trabalho.",
+                    "A alimentação balanceada e o sono estão interligados para a saúde geral."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Alimentos ricos em triptofano podem promover um sono melhor.",
+                afirmacao: [
+                    "Consultar um profissional de saúde pode ser necessário se houver problemas persistentes com o sono.",
+                    "Dormir bem está relacionado à melhora do estado emocional e à redução da irritabilidade."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Quais são os efeitos positivos de técnicas de relaxamento antes de dormir?",
+        alternativas: [
+            {
+                texto: "Ajudam a reduzir o estresse e a ansiedade.",
+                afirmacao: [
+                    "O sono adequado pode auxiliar na perda de peso e na manutenção de um peso saudável.",
+                    "Manter uma postura adequada ao dormir pode prevenir dores e desconfortos físicos."
+                ],
+            },
+            {
+                texto: "Facilitam a transição para um sono mais profundo e reparador.",
+                afirmacao: [
+                    "Evitar pensamentos negativos antes de dormir pode melhorar a qualidade do sono.",
+                    "Priorizar o sono é uma forma de cuidar da saúde mental e física.",
+                    ""
+                ],
+            },
+        ]
+    }
+]
